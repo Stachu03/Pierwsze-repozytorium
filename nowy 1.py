b46b5ad0f75136c3420bd97ac6ba9706ae7df215
Stahu03 <stas.kornacki@onet.pl>
2021-02-23 13:12:27 +0100

    Piewsze próby z Atom

---
+++ nowy 1.py
@@ -0,0 +1,7 @@
+# nowy plik w repozytorium
+import pgzrun
+WIDTH = 1280
+HEIGHT = 853
+def draw():
+    screen.blit("desert-1654439_1280.jpg",(0,0))
+pgzrun.go()
---
+++ nowy atom.py
@@ -0,0 +1,5 @@
+# nowy plik w repozytorium
+import pgzrun
+WIDTH = 1280
+HEIGHT = 853
+pgzrun.go()
